ddadd45df1a76acf2fc7f02df99733406a838e93
David Horstman <davidzilla27@gmail.com>
2020-04-16 17:56:43 -0400

    Done!

--- src/components/LotRContainer.js
+++ src/components/LotRContainer.js
@@ -1,19 +1,38 @@
 import React from "react";
 
 import { connect } from "react-redux";
-import { fetchData } from "../actions";
+import { fetchData, fetchQuotes } from "../actions";
+
+import CharacterCard from "./CharacterCard";
+import { Container, Button, Spinner } from "reactstrap";
 
 function LotrContainer(props) {
+  //React.useEffect(props.fetchData, []);
   return (
-    <>
-      <button onClick={props.fetchData}>Fetch Data</button>
-    </>
+    <div style={{backgroundColor:"lightgrey"}}>
+      <h1>Lord of the Rings Characters</h1>
+      <Container xs="2">
+        {props.isLoading ? (
+          <Spinner />
+        ) : (
+          <Button onClick={props.fetchData}>Fetch Data</Button>
+        )}
+        {props.books &&
+          props.books.map((x) => (
+            <CharacterCard
+              key={x._id}
+              {...x}
+              fetchQuotes={() => props.fetchQuotes(x._id)}
+            />
+          ))}
+      </Container>
+    </div>
   );
 }
 
 export default connect(
-  (state) => {
+  state => {
     return { ...state };
   },
-  { fetchData }
+  { fetchData, fetchQuotes }
 )(LotrContainer);
